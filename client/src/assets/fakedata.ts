0db6a062049155b8ebab6e0a0eb5690b94d74e11
skies-codes <akashdebnathwd@gmail.com>
2024-10-02 15:24:49 +0530

    Created Component - Sidebar

---
+++ client/src/assets/fakedata.ts
@@ -0,0 +1,27 @@
+interface Project {
+  id: string;
+  name: string;
+}
+
+export const projects: Project[] = [
+  {
+    id: "1",
+    name: "Project Alpha",
+  },
+  {
+    id: "2",
+    name: "Website Redesign",
+  },
+  {
+    id: "3",
+    name: "Content Management System",
+  },
+  {
+    id: "4",
+    name: "Data Analytics Dashboard",
+  },
+  {
+    id: "5",
+    name: "Graphic Design Portfolio",
+  },
+];
---
+++ client/src/components/Sidebar/index.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import {
+  AlertCircle,
+  AlertOctagon,
+  AlertTriangle,
+  Briefcase,
+  ChevronDown,
+  ChevronUp,
+  Home,
+  Layers3,
+  LockIcon,
+  LucideIcon,
+  Search,
+  Settings,
+  ShieldAlert,
+  User,
+  Users,
+  X,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
+import SidebarLink from "./SidebarLink";
+import { projects } from "@/assets/fakedata";
+
+const Sidebar = () => {
+  const [showProjects, setShowProjects] = useState(true);
+  const [showPriority, setShowPriority] = useState(true);
+
+  const isSidebarCollapsed = false;
+  const profilepic =
+    "https://imgs.search.brave.com/ry6KvTiHmjjX7a0CqfC5zcrrn3o4Njaq46zrdyznPXs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvc2Fk/LWJsb25kLWN1dGUt/YW5pbWUtZ2lybC1w/ZnAtaGNiem5leTdi/cGFvZmQxay5qcGc";
+
+  const handleSignOut = async () => {
+    console.log("sign out");
+  };
+
+  const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
+      transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white
+      ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}
+    `;
+  return (
+    <aside className={sidebarClassNames}>
+      <div className="flex h-[100%] w-full flex-col justify-start">
+        {/* TOP LOGO  */}
+        <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
+          <div className="text-xl font-bold text-gray-800 dark:text-white">
+            AKLIST
+          </div>
+          {isSidebarCollapsed ? null : (
+            <button className="py-3">
+              <X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white" />
+            </button>
+          )}
+        </div>
+
+        {/* TEAM  */}
+        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+          <Image src={profilepic} alt="Logo" width={40} height={40} />
+          <div>
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              AKASH TEAM
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Navbar Links  */}
+        <nav className="z-10 w-full">
+          <SidebarLink icon={Home} label="Home" href="/" />
+          <SidebarLink icon={Briefcase} label="Timeline" href="/timeline" />
+          <SidebarLink icon={Search} label="Search" href="/search" />
+          <SidebarLink icon={User} label="Users" href="/users" />
+          <SidebarLink icon={Users} label="Teams" href="/teams" />
+          <SidebarLink icon={Settings} label="Settings" href="/settings" />
+        </nav>
+
+        {/* Project Links  */}
+        <button
+          onClick={() => setShowProjects((prev) => !prev)}
+          className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+        >
+          <span className="">Projects</span>
+          {showProjects ? (
+            <ChevronUp className="h-5 w-5" />
+          ) : (
+            <ChevronDown className="h-5 w-5" />
+          )}
+        </button>
+
+        {/* Project List  */}
+        {showProjects &&
+          projects?.map((project) => (
+            <SidebarLink
+              key={project.id}
+              icon={Briefcase}
+              label={project.name}
+              href={`/projects/${project.id}`}
+            />
+          ))}
+
+        {/* PRIORITIES LINKS */}
+        <button
+          onClick={() => setShowPriority((prev) => !prev)}
+          className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+        >
+          <span className="">Priority</span>
+          {showPriority ? (
+            <ChevronUp className="h-5 w-5" />
+          ) : (
+            <ChevronDown className="h-5 w-5" />
+          )}
+        </button>
+
+        {/* PRIORITIES List */}
+        {showPriority && (
+          <>
+            <SidebarLink
+              icon={AlertCircle}
+              label="Urgent"
+              href="/priority/urgent"
+            />
+            <SidebarLink
+              icon={ShieldAlert}
+              label="High"
+              href="/priority/high"
+            />
+            <SidebarLink
+              icon={AlertTriangle}
+              label="Medium"
+              href="/priority/medium"
+            />
+            <SidebarLink icon={AlertOctagon} label="Low" href="/priority/low" />
+            <SidebarLink
+              icon={Layers3}
+              label="Backlog"
+              href="/priority/backlog"
+            />
+          </>
+        )}
+      </div>
+      <div className="z-10 mt-32 flex w-full flex-col items-center gap-4 bg-white px-8 py-4 dark:bg-black md:hidden">
+        <div className="flex w-full items-center">
+          <div className="align-center flex h-9 w-9 justify-center">
+            {profilepic ? (
+              <Image
+                src={profilepic}
+                alt={"User Profile Picture"}
+                width={100}
+                height={50}
+                className="h-full rounded-full object-cover"
+              />
+            ) : (
+              <User className="h-6 w-6 cursor-pointer self-center rounded-full dark:text-white" />
+            )}
+          </div>
+          <span className="mx-3 text-gray-800 dark:text-white">Akash</span>
+          <button
+            className="self-start rounded bg-blue-400 px-4 py-2 text-xs font-bold text-white hover:bg-blue-500 md:block"
+            onClick={handleSignOut}
+          >
+            Sign out
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
