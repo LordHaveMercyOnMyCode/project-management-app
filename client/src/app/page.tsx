4837f052d6041a02c1b65f62e307117cd4333888
skies-codes <akashdebnathwd@gmail.com>
2024-10-04 21:46:14 +0530

    Added new page projects, and components Header, Modal, ModalNewTask

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex h-full w-full items-center justify-center">
-      <h1 className="font-mono text-xl">hello world</h1>
+    <main className="flex h-full w-full items-center justify-center bg-gray-50 dark:bg-dark-secondary">
+      <h1 className="font-mono text-xl text-black dark:text-white">
+        hello world
+      </h1>
     </main>
   );
 }
---
+++ client/src/app/projects/[id]/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import ModalNewTask from "@/components/ModalNewTask";
+import { useState } from "react";
+import ProjectHeader from "../ProjectHeader";
+
+type Props = {
+  params: { id: string };
+};
+
+const Project = ({ params }: Props) => {
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
+  return (
+    <section>
+      <ModalNewTask
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+        id={id}
+      />
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+    </section>
+  );
+};
+
+export default Project;
