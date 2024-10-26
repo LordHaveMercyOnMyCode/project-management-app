1a17d64e9b83e1114dc9f3bb07b27ef55b155259
LordHaveMercyOnMyCode <akashdebnathwd@gmail.com>
2024-10-26 12:18:05 +0530

    Modified the server/src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -36,9 +36,7 @@ app.use("/teams", teamRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
-// app.listen(port, "0.0.0.0", () => {
-//     console.log(`Server running on part ${port}`);
-// });
-app.listen(port, () => {
+
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on part ${port}`);
 });
